861c91642d2ce0fbce7c702c4437073663d418cd
Pedro Vieira <pedro.silva@pagtel.com.br>
2023-10-19 10:16:53 -0300

    feat: add button component

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,66 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from '@pedrovs3/react/src'
+import { ArrowRight } from 'phosphor-react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  argTypes: {
+    disabled: {
+      type: 'boolean',
+    },
+    variant: {
+      control: 'select',
+      options: ['primary', 'secondary', 'outline', 'ghost'],
+    },
+    size: {
+      control: 'select',
+      options: ['sm', 'md'],
+    },
+  },
+  args: {
+    children: 'Send',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+  },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+  },
+}
+
+export const Ghost: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'ghost',
+  },
+}
+
+export const Outline: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'outline',
+  },
+}
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
+  },
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Send
+        <ArrowRight weight={'bold'} />
+      </>
+    ),
+  },
+}
---
+++ packages/react/src/components/Button.tsx
@@ -0,0 +1,85 @@
+import { styled } from '../styles'
+
+export const Button = styled('button', {
+  appearance: 'none',
+  fontFamily: '$default',
+  fontSize: '$sm',
+  lineHeight: '$shorter',
+  textAlign: 'center',
+  fontWeight: '$medium',
+  borderRadius: '$md',
+  border: 'none',
+  boxSizing: 'border-box',
+  minWidth: 120,
+  cursor: 'pointer',
+  transition: 'all 0.2s ease-in-out',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '$2',
+
+  '&:not(:disabled):hover': {
+    color: '$gray100',
+    backgroundColor: '$ignite700',
+  },
+
+  '&:disabled': {
+    opacity: 0.5,
+    cursor: 'not-allowed',
+  },
+
+  svg: {
+    width: '$4',
+    height: '$4',
+  },
+
+  variants: {
+    variant: {
+      primary: {
+        backgroundColor: '$ignite500',
+        color: '$gray100',
+        '&:not(:disabled):hover': {
+          backgroundColor: '$ignite700',
+        },
+      },
+      secondary: {
+        backgroundColor: '$gray500',
+        color: '$gray100',
+        '&:not(:disabled):hover': {
+          backgroundColor: '$gray700',
+        },
+      },
+      outline: {
+        border: '2px solid $ignite500',
+        color: '$ignite500',
+      },
+      ghost: {
+        backgroundColor: 'transparent',
+        border: 'none',
+        '&:not(:disabled):hover': {
+          backgroundColor: '$ignite700',
+        },
+      },
+    },
+    size: {
+      sm: {
+        padding: '0 $2',
+        height: 32,
+        fontSize: '$sm',
+      },
+      md: {
+        padding: '0 $4',
+        height: 46,
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+  },
+})
+
+export interface ButtonProps extends React.ComponentProps<typeof Button> {
+  as?: React.ElementType
+}
