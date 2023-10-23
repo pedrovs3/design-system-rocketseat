ee230170874cb8b2fdd6388f1b615cdba9ab3520
Pedro Vieira <pedro.silva@pagtel.com.br>
2023-10-23 09:13:31 -0300

    fix: fix width for component Box

--- packages/react/src/components/Box.tsx
+++ packages/react/src/components/Box.tsx
@@ -5,8 +5,6 @@ export const Box = styled('div', {
   borderRadius: '$md',
   backgroundColor: '$gray800',
   border: '1px solid $gray600',
-  width: '100%',
-  maxWidth: '100%',
 })
 
 export type BoxProps = React.ComponentProps<typeof Box>
