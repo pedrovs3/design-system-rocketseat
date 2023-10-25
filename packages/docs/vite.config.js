ed78a43fb17f3655ec92d36c85ec1c4f7bee2ebb
Pedro Vieira <pedro.silva@pagtel.com.br>
2023-10-25 08:32:33 -0300

    test: deploy

--- packages/docs/vite.config.js
+++ packages/docs/vite.config.js
@@ -5,4 +5,15 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions:{
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
 })
